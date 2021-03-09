da50d132ab4ba347c77f828e7c0461f340fafbc1
mehikajuyal <45632017+mehikajuyal@users.noreply.github.com>
2021-03-10 03:14:46 +0530

    Update alphaprocessor.js  added  and testcases updated
    Updated alphaprocessor.js  and  made the testcases success. 
    I saw your comment quite late so just updated it.

--- alphaProcessor.js
+++ alphaProcessor.js
@@ -3,8 +3,44 @@ const _ = require('lodash');
 const winston = require('winston');
 
 const processData = (data) => {
-    let finalData = [];
-
+    // let finalData = [];
+    let teacher = [];
+    let student = [];
+    data.members.forEach((x) => {
+        if(x.teacher_id === null){
+          teacher = [...teacher, x]
+        }else{
+            student = [...student,x]
+        }
+    })
+    teacher.forEach((y) => {
+        let stud = [];
+         stud = student.filter((x) => {
+           return x.teacher_id === y.id;
+        })
+        let t=0;
+      let  s = stud.map(item => item.marks? item.marks: 0).reduce((prev, next) => prev + next);
+      const $stud = stud.sort((a, b) => {
+        return b.marks - a.marks;
+    });
+        y['students'] = stud;
+        y['marks']= s
+    })
+ data.schools.forEach((s) => {
+     let memb = [];
+     memb = teacher.filter((t) => {
+         return s.id === t.school_id;
+     })
+     let  m = memb.map(item => item.marks? item.marks: 0).reduce((prev, next) => prev + next);
+     const mem = memb.sort((a, b) => {
+        return b.marks - a.marks;
+    });
+     s['members'] = memb;
+     s['marks']= m
+ })
+ const finalData = data.schools.sort((a, b) => {
+   return b.marks - a.marks;
+});
     return finalData;
 }
 

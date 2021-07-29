c601ea8e2d9d9342fbeeafd48b2e800376df2145
itmonksakshay <akshay.bhatt@hotmail.com>
2021-07-29 15:09:27 +0530

    beta test success

--- beta/beta-processor.js
+++ beta/beta-processor.js
@@ -4,20 +4,54 @@ const winston = require('winston');
 
 const maxMarks = 50;
 
+
+const groupBy =(array,key)=>{
+	let newArray = array.reduce((result, currentValue) => {
+    	(result[currentValue[key]] = result[currentValue[key]] || []).push(
+			currentValue);
+    	return result;
+	},{});
+	
+
+	let objArray = [];
+	Object.keys(newArray).forEach(key => objArray.push({
+	   name: key,students: newArray[key]
+	}));
+
+
+	return objArray 
+};
+
+
+
+
+
 const processData = (data) => {
-    let finalData = [];
+	//InitialProcessing of Data
+	let processedData = data.map(student => {
+		return {...student,title:student.name.first +' '+student.name.last,
+			totalMarks: student.marks.reduce ( (totalMarks, subject) => {
+		
+    			return totalMarks +=(((subject.marks / maxMarks) * 100) >= 33)
+					? subject.marks:0;
+        	}, 0)}
 
-    data = data.map(student => {
-        student.totalMarks = student.marks.reduce ( (totalMarks, subject) => {
-            totalMarks += (((subject.marks / maxMarks) * 100) >= 33) ? subject.marks : 0;
-        }, 0);
+    }).sort((studentA,studentB)=>{
+		return studentB.totalMarks -studentA.totalMarks;
+	});
 
-        return student;
-    });
+	// top marks Values 
+	let studentSort = processedData.map(student=>{
+		let tempStudent = student.title+' from '+student.class
+			+' obtained '+student.totalMarks
+		return tempStudent;
+	});
 
-    let classes =  _.groupBy(data, 'class');
 
-    return finalData;
+	let intState= groupBy(processedData,'class');
+
+	let finalArray = {records:intState,top:studentSort}; 
+	return finalArray;
 }
 
 module.exports = {
